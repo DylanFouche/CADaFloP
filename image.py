d212e03ec41a6bdce9eaf7fe10b66e2507b0763d
Dylan Fouche <dylanfouche@gmail.com>
2020-05-27 18:04:02 +0000

    Removed support for fits because astropy.io is really dumb with large files

--- image.py
+++ image.py
@@ -3,7 +3,6 @@
 # fchdyl001@myuct.ac.za
 
 import numpy
-from astropy.io import fits
 from h5py import File as hdf5
 import dask.array as da
 
@@ -11,33 +10,31 @@ class image:
     def __init__(self):
         self.filename = ""
         self.filetype = ""
-        self.dimensions = 0
         self.shape = []
+        self.dimensions = 0
         self.data = null
 
     def __init__(self,filename, chunk=1000):
         self.filename = filename
         self.filetype = filename[self.filename.rfind('.')+1:]
 
-        if(self.filetype == "hdf5"):
-            f = hdf5(self.filename, 'r')
-            if 'DATA' in f['0']:
-                d = f['0']['DATA']
-                self.dimensions = len(d.shape)
-                self.shape = d.shape
-                self.data = da.from_array(d, chunks=([chunk] * self.dimensions))
-            else:
-                print("Unable to read in hdf5 file")
+        try:
+            if (self.filetype != "hdf5"):
+                raise Exception("Unsupported file type.")
 
-        elif(self.filetype == "fits"):
-            f = fits.open(self.filename, memmap=True)
-            d = f[0].data
-            self.dimensions = len(d.shape)
+            f = hdf5(self.filename, 'r')
+
+            if 'DATA' not in f['0']:
+                raise Exception("Unexpected format in hdf file.")
+
+            d = f['0']['DATA']
             self.shape = d.shape
+            self.dimensions = len(self.shape)
             self.data = da.from_array(d, chunks=([chunk] * self.dimensions))
 
-        else:
-            print("Filetype %s not supported" %self.filetype)
+        except Exception as e:
+            print("Unable to read in image file:")
+            print(str(e))
 
     def __getitem__(self,key):
         return self.data[key]
