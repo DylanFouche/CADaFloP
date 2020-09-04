ed96855f001785301f397ec249aa2560d6793718
Dylan Fouche <dylanfouche@gmail.com>
2020-09-04 22:53:44 +0200

    Add util/comms message utilities

---
+++ src/util/comms.py
@@ -0,0 +1,42 @@
+# D FOUCHE
+# UCT CS HONS
+# fchdyl001@myuct.ac.za
+
+# Reference: https://krpc.github.io/krpc/communication-protocols/tcpip.html
+
+from google.protobuf.internal.encoder import _VarintEncoder
+from google.protobuf.internal.decoder import _DecodeVarint
+
+def encode_varint(value):
+    """ Encode an int as a protobuf varint """
+    data = []
+    _VarintEncoder()(data.append, value, False)
+    return b''.join(data)
+
+def decode_varint(data):
+    """ Decode a protobuf varint to an int """
+    return _DecodeVarint(data, 0)[0]
+
+def send_message(conn, msg):
+    """ Send a message, prefixed with its size, to a TPC/IP socket """
+    data = msg.SerializeToString()
+    size = encode_varint(len(data))
+    conn.sendall(size + data)
+
+def recv_message(conn, msg_type):
+    """ Receive a message, prefixed with its size, from a TCP/IP socket """
+    # Receive the size of the message data
+    data = b''
+    while True:
+        try:
+            data += conn.recv(1)
+            size = decode_varint(data)
+            break
+        except IndexError:
+            pass
+    # Receive the message data
+    data = conn.recv(size)
+    # Decode the message
+    msg = msg_type()
+    msg.ParseFromString(data)
+    return msg
