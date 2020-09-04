ae4b99a6e02a8f32271afd5faa9c5693ac5b39eb
Dylan Fouche <dylanfouche@gmail.com>
2020-09-04 22:54:09 +0200

    Exchange REGISTER_VIEWER and REGISTER_VIEWER_ACK

--- src/backend/server.py
+++ src/backend/server.py
@@ -5,7 +5,10 @@
 import socket
 import logging
 
+import numpy as np
+
 from src.protobuf import register_viewer_pb2
+from src.util.comms import *
 
 class Server:
     def __init__(self, port=3003):
@@ -19,6 +22,17 @@ class Server:
         except:
             logging.error("\t[Server]\tUnable to listen on port %s.", port)
 
+        clientSocket, clientAddress = serverSocket.accept()
+        logging.info("\t[Server]\tConnection from %s has been established.", clientAddress)
+
         while True:
-            clientSocket, clientAddress = serverSocket.accept()
-            logging.info("\t[Server]\tConnection from %s has been established.", clientAddress)
+            try:
+                msg = recv_message(clientSocket, register_viewer_pb2.RegisterViewer)
+                logging.info("\t[Server]\tClient %s registered as a viewer with id %s.", clientAddress, msg.session_id)
+
+                ack = register_viewer_pb2.RegisterViewerAck()
+                ack.session_id = msg.session_id
+                send_message(clientSocket, ack)
+                logging.info("\t[Server]\tSent REGISTER_VIEWER_ACK for id %s to client %s.", ack.session_id, clientAddress)
+            except:
+                logging.error("\t[Server]\tUnable to process incoming message from client %s.", clientAddress)
--- src/frontend/client.py
+++ src/frontend/client.py
@@ -4,8 +4,13 @@
 
 import socket
 import logging
+import time
+import uuid
+
+import numpy as np
 
 from src.protobuf import register_viewer_pb2
+from src.util.comms import *
 
 class Client:
     def __init__(self, port=3003):
@@ -17,3 +22,17 @@ class Client:
             logging.info("\t[Client]\tConnected to server on %s successfully.", port)
         except:
             logging.error("\t[Client]\tUnable to connect to server on %s.", port)
+
+        while True:
+            msg = register_viewer_pb2.RegisterViewer()
+            msg.session_id = np.uint32(uuid.uuid4().int % np.iinfo(np.uint32()).max)
+
+            try:
+                send_message(clientSocket, msg)
+                logging.info("\t[Client]\tSent REGISTER_VIEWER with id %s to server on %s.", msg.session_id, port)
+                ack = recv_message(clientSocket, register_viewer_pb2.RegisterViewerAck)
+                logging.info("\t[Client]\tGot REGISTER_VIEWER_ACK with id %s from server on %s.", ack.session_id, port)
+            except:
+                logging.error("\t[Client]\tUnable to register as a viewer with server on %s.", port)
+
+            time.sleep(5)
