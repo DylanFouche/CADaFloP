7d37ec663e0d1937035cf2060620fe8012908167
Dylan Fouche <dylanfouche@gmail.com>
2020-10-01 14:00:21 +0200

    Update graphing tool to use four subplots

--- analysis/graph_it.py
+++ analysis/graph_it.py
@@ -8,51 +8,76 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-for file in sys.argv[1:]:
+xs = [str(n) for n in range(1,21)]
 
-    xs = [str(n) for n in range(1000, 21000, 1000)]
+means = []
 
-    means = []
+with open(sys.argv[1], "r") as f:
+    for line in f:
+        if line[0:5] == "Mean:":
+            means.append(float(line[6:].rstrip()))
 
-    with open(file, "r") as f:
-        for line in f:
-            if line[0:5] == "Mean:":
-                means.append(float(line[6:].rstrip()))
+carta_histo_disk = means[0:20]
+carta_stats_disk = means[20:40]
+dask_local_histo_disk = means[40:60]
+dask_local_stats_disk = means[60:80]
+dask_distributed_histo_disk = means[80:100]
+dask_distributed_stats_disk = means[100:120]
 
-    carta_histo = means[0:20]
-    carta_stats = means[20:40]
+means = []
 
-    dask_local_histo = means[40:60]
-    dask_local_stats = means[60:80]
+with open(sys.argv[2], "r") as f:
+    for line in f:
+        if line[0:5] == "Mean:":
+            means.append(float(line[6:].rstrip()))
 
-    dask_distributed_histo = means[80:100]
-    dask_distributed_stats = means[100:120]
+carta_histo_ram = means[0:20]
+carta_stats_ram = means[20:40]
+dask_local_histo_ram = means[40:60]
+dask_local_stats_ram = means[60:80]
+dask_distributed_histo_ram = means[80:100]
+dask_distributed_stats_ram = means[100:120]
 
-    fig, axs = plt.subplots(2)
+fig, axs = plt.subplots(2,2)
 
-    fig.suptitle('CADaFloP Performance Test Results')
+fig.suptitle('CADaFloP Performance Test Results')
 
-    x = np.arange(len(xs))
-    width = 0.25
+x = np.arange(len(xs))
+width = 0.25
 
-    axs[0].bar(x - width, carta_histo, width, label='CARTA')
-    axs[0].bar(x, dask_local_histo, width, label='Dask (local)')
-    axs[0].bar(x + width, dask_distributed_histo, width, label='Dask (distributed)')
-    axs[0].set_xlabel("Image dimensions (n X n pixels)")
-    axs[0].set_ylabel("Time (s)")
-    axs[0].set_xticks(x)
-    axs[0].set_xticklabels(xs)
-    axs[0].legend()
-    axs[0].set_title("Region histogram computation")
+axs[0,0].bar(x - width, carta_histo_disk, width, label='CARTA')
+axs[0,0].bar(x, dask_local_histo_disk, width, label='Dask (local)')
+axs[0,0].bar(x + width, dask_distributed_histo_disk, width, label='Dask (distributed)')
+axs[0,0].set_xticks(x)
+axs[0,0].set_xticklabels(xs)
+axs[0,0].legend()
+axs[0,0].set_title("(a) region histogram computation (from disk)")
 
-    axs[1].bar(x - width, carta_stats, width, label='CARTA')
-    axs[1].bar(x, dask_local_stats, width, label='Dask (local)')
-    axs[1].bar(x + width, dask_distributed_stats, width, label='Dask (distributed)')
-    axs[1].set_xlabel("Image dimensions (n X n pixels)")
-    axs[1].set_ylabel("Time (s)")
-    axs[1].set_xticks(x)
-    axs[1].set_xticklabels(xs)
-    axs[1].legend()
-    axs[1].set_title("Region statistics computation")
+axs[0,1].bar(x - width, carta_histo_ram, width, label='CARTA')
+axs[0,1].bar(x, dask_local_histo_ram, width, label='Dask (local)')
+axs[0,1].bar(x + width, dask_distributed_histo_ram, width, label='Dask (distributed)')
+axs[0,1].set_xticks(x)
+axs[0,1].set_xticklabels(xs)
+axs[0,1].legend()
+axs[0,1].set_title("(b) region histogram computation (from memory)")
 
-    plt.show()
+axs[1,0].bar(x - width, carta_stats_disk, width, label='CARTA')
+axs[1,0].bar(x, dask_local_stats_disk, width, label='Dask (local)')
+axs[1,0].bar(x + width, dask_distributed_stats_disk, width, label='Dask (distributed)')
+axs[1,0].set_xticks(x)
+axs[1,0].set_xticklabels(xs)
+axs[1,0].legend()
+axs[1,0].set_title("(c) region statistics computation (from disk)")
+
+axs[1,1].bar(x - width, carta_stats_ram, width, label='CARTA')
+axs[1,1].bar(x, dask_local_stats_ram, width, label='Dask (local)')
+axs[1,1].bar(x + width, dask_distributed_stats_ram, width, label='Dask (distributed)')
+axs[1,1].set_xticks(x)
+axs[1,1].set_xticklabels(xs)
+axs[1,1].legend()
+axs[1,1].set_title("(d) region statistics computation (from memory)")
+
+for ax in axs.flat:
+    ax.set(xlabel="Image size (1000n X 1000n pixels)", ylabel="Time (s)")
+
+plt.show()
