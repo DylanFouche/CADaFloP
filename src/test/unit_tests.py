f41549630547960f745d51b06cea0366d044db2f
Dylan Fouche <dylanfouche@gmail.com>
2020-10-15 19:39:29 +0200

    Refactor is_close function

--- src/test/unit_tests.py
+++ src/test/unit_tests.py
@@ -40,27 +40,6 @@ class UnitTests(unittest.TestCase):
         self.dask_client.register_viewer()
         self.carta_client.register_viewer()
 
-    def is_close(self, a, b):
-        """Assert that two numbers or lists are almost equal (accounting for floating point errors).
-
-        :param a: the first value or iterable
-        :param b: the second value or iterable
-        :return: True if the two values or iterables are close, False otherwise
-
-        """
-        if a == b:
-            return True
-        if isinstance(a, Iterable) and isinstance(b, Iterable):
-            for ai, bi in zip(a, b):
-                if bi > (ai + (0.001 * ai)) or bi < (ai - (0.001 * ai)):
-                    raise AssertionError(
-                        "{} is not close to {}".format(ai, bi))
-        else:
-            if b > (a + (0.001 * a)) or b < (a - (0.001 * a)):
-                raise AssertionError(
-                    "{} is not close to {}".format(a, b))
-        return True
-
     def test_histogram_bins_correct(self):
         """Assert that region histogram bins returned by carta and dask are close."""
         for test_file in self.TEST_FILES:
@@ -70,7 +49,7 @@ class UnitTests(unittest.TestCase):
                 dask_histo, dask_mean, dask_std = self.dask_client.get_region_histogram()
                 carta_histo, carta_mean, carta_std = self.carta_client.get_region_histogram()
 
-                self.assertTrue(self.is_close(dask_histo, carta_histo))
+                self.assertTrue(is_close(dask_histo, carta_histo))
 
     def test_histogram_mean_correct(self):
         """Assert that region histogram means returned by carta and dask are close."""
@@ -81,7 +60,7 @@ class UnitTests(unittest.TestCase):
                 dask_histo, dask_mean, dask_std = self.dask_client.get_region_histogram()
                 carta_histo, carta_mean, carta_std = self.carta_client.get_region_histogram()
 
-                self.assertTrue(self.is_close(dask_mean, carta_mean))
+                self.assertTrue(is_close(dask_mean, carta_mean))
 
     def test_histogram_standard_deviation_correct(self):
         """Assert that region histogram standard deviations returned by carta and dask are close."""
@@ -92,7 +71,7 @@ class UnitTests(unittest.TestCase):
                 dask_histo, dask_mean, dask_std = self.dask_client.get_region_histogram()
                 carta_histo, carta_mean, carta_std = self.carta_client.get_region_histogram()
 
-                self.assertTrue(self.is_close(dask_std, carta_std))
+                self.assertTrue(is_close(dask_std, carta_std))
 
     def test_stats_sum_correct(self):
         """Assert that region statistics sum returned by carta and dask are close."""
@@ -103,7 +82,7 @@ class UnitTests(unittest.TestCase):
                 dask_stats = self.dask_client.get_region_statistics()
                 carta_stats = self.carta_client.get_region_statistics()
 
-                self.assertTrue(self.is_close(dask_stats[0], carta_stats[0]))
+                self.assertTrue(is_close(dask_stats[0], carta_stats[0]))
 
     def test_stats_mean_correct(self):
         """Assert that region statistics mean returned by carta and dask are close."""
@@ -114,7 +93,7 @@ class UnitTests(unittest.TestCase):
                 dask_stats = self.dask_client.get_region_statistics()
                 carta_stats = self.carta_client.get_region_statistics()
 
-                self.assertTrue(self.is_close(dask_stats[1], carta_stats[1]))
+                self.assertTrue(is_close(dask_stats[1], carta_stats[1]))
 
     def test_stats_sigma_correct(self):
         """Assert that region statistics sigma returned by carta and dask are close."""
@@ -125,7 +104,7 @@ class UnitTests(unittest.TestCase):
                 dask_stats = self.dask_client.get_region_statistics()
                 carta_stats = self.carta_client.get_region_statistics()
 
-                self.assertTrue(self.is_close(dask_stats[2], carta_stats[2]))
+                self.assertTrue(is_close(dask_stats[2], carta_stats[2]))
 
     def test_stats_min_correct(self):
         """Assert that region statistics min returned by carta and dask are close."""
@@ -136,7 +115,7 @@ class UnitTests(unittest.TestCase):
                 dask_stats = self.dask_client.get_region_statistics()
                 carta_stats = self.carta_client.get_region_statistics()
 
-                self.assertTrue(self.is_close(dask_stats[3], carta_stats[3]))
+                self.assertTrue(is_close(dask_stats[3], carta_stats[3]))
 
     def test_stats_max_correct(self):
         """Assert that region statistics max returned by carta and dask are close."""
@@ -147,4 +126,28 @@ class UnitTests(unittest.TestCase):
                 dask_stats = self.dask_client.get_region_statistics()
                 carta_stats = self.carta_client.get_region_statistics()
 
-                self.assertTrue(self.is_close(dask_stats[4], carta_stats[4]))
+                self.assertTrue(is_close(dask_stats[4], carta_stats[4]))
+
+
+def is_close(a, b):
+    """Assert that two numbers or lists are almost equal (accounting for floating point errors).
+
+    :param a: the first value or iterable
+    :param b: the second value or iterable
+    :return: True if the two values or iterables are close, False otherwise
+
+    """
+    if a == b:
+        return True
+    if isinstance(a, Iterable) and isinstance(b, Iterable):
+        for ai, bi in zip(a, b):
+            if ai == bi:
+                continue
+            if bi > (ai + (0.001 * ai)) or bi < (ai - (0.001 * ai)):
+                raise AssertionError(
+                    "{} is not close to {}".format(ai, bi))
+    else:
+        if b > (a + (0.001 * a)) or b < (a - (0.001 * a)):
+            raise AssertionError(
+                "{} is not close to {}".format(a, b))
+    return True
